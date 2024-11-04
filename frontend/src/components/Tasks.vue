a505b8dda745451affd667a540704d460755846b
Wojciech Faber <tazor1337@gmail.com>
2024-11-04 15:11:15 +0100

    Frontend Config
    - vue + vite
    - pinia + axios
    - tailwind css + postcss + autoprefixer
    - pretteir
    - typescript

---
+++ frontend/src/components/Tasks.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>
+		<h2>Lista Zadań</h2>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+	name: 'Tasks',
+});
+</script>
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+	<div>
+		<h1>Witaj w aplikacji To Do List!</h1>
+		<p>To jest prosty przykład strony głównej z Tailwind CSS.</p>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+	name: 'Home',
+});
+</script>
+
+<style scoped></style>
